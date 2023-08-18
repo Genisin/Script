3166c3452c69338c71358319273c82957fbfb346
Genisin <1841542034@qq.com>
2023-08-18 21:01:31 +0800

    v1

--- Log_Clear/LCInstall_T.sh
+++ Log_Clear/LCInstall_T.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #依赖下载安装和主脚本下载
-#wget -O Dyinstall.sh https://github.com/Genisin/script/raw/main/Log_Clear/LCInstall_T.sh && chmod +x Dyinstall.sh && sudo ./Dyinstall.sh
+#  wget -O Dyinstall.sh https://github.com/Genisin/script/raw/main/Log_Clear/LCInstall_T.sh && chmod +x Dyinstall.sh && sudo ./Dyinstall.sh
  
 # 定义脚本名变量
 script_name="logclear.sh"
@@ -14,7 +14,7 @@ download_path="/root/data/script"
 #替换域名raw.githubusercontent.com
 wget -O "$download_path/$script_name" https://github.com/Genisin/script/raw/main/Log_Clear/main.sh && chmod +x "$download_path/$script_name"
 
-echo "所需依赖已全部安装成功，请输入sudo $download_path/$script_name运行"
+echo "所需依赖已全部安装成功，请输入-> sudo $download_path/$script_name <-运行"
 
 #删除此脚本（此脚本是为检测依赖是否安装并下载主脚本）
 rm "$0"
