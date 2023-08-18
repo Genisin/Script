b134e6f3cbd11a89839fe441516d37bbb1c0c96c
Genisin <110389722+Genisin@users.noreply.github.com>
2023-08-18 17:10:09 +0800

    Update SIInstall.sh

--- System_Info/SIInstall.sh
+++ System_Info/SIInstall.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-#依赖下载安装和主脚本下载
-#wget -O Dyinstall.sh https://github.com/Genisin/script/edit/main/System_Info/SIInstall.sh && chmod +x Dyinstall.sh && sudo ./Dyinstall.sh
+#依赖下载安装和主脚本下载！！！ 替换域名为 -> raw.githubusercontent.com <-
+#wget -O Dyinstall.sh https://raw.githubusercontent.com/Genisin/script/edit/main/System_Info/SIInstall.sh && chmod +x Dyinstall.sh && sudo ./Dyinstall.sh
  
-# 定义脚本名变量
-script_name="logclear.sh"
+# 脚本名字
+script_name="System_Info.sh"
+#脚本下载地址 
+main_script_url= "https://raw.githubusercontent.com/Genisin/script/blob/main/System_Info/main.sh"
 
 #创建主脚本下载路径
 mkdir -p /root/data/script
 download_path="/root/data/script"
 
 #下载主脚本到指定文件夹并赋予执行权限
-#替换域名raw.githubusercontent.com
-wget -O "$download_path/$script_name" https://raw.githubusercontent.com/Genisin/script/blob/main/System_Info/main.sh && chmod +x "$download_path/$script_name"
+wget -O "$download_path/$script_name" "$main_script_url" && chmod +x "$download_path/$script_name"
 
-echo "所需依赖已全部安装成功，请输入sudo $download_path/$script_name运行"
-
+echo "所需依赖已全部安装成功，此脚本即将自动删除"
+echo "请输入-> sudo $download_path/$script_name  <-进行运行所需脚本"
 #删除此脚本（此脚本是为检测依赖是否安装并下载主脚本）
 rm "$0"
