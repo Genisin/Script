b46f6345dffe1a728a4e6ba23d88deee8ba5d62d
Genisin <1841542034@qq.com>
2023-08-19 15:28:21 +0800

    v1

--- System_Info/SIInstall.sh
+++ System_Info/SIInstall.sh
@@ -7,18 +7,18 @@
 # 脚本名字
 script_name="System_Info.sh"
 #脚本下载地址 
-main_script_url= "https://raw.githubusercontent.com/Genisin/script/main/System_Info/main.sh"
+main_script_url="https://raw.githubusercontent.com/Genisin/script/main/System_Info/main.sh"
 
 #创建主脚本下载路径
 mkdir -p /root/data/script
 download_path="/root/data/script"
 
 #下载主脚本到指定文件夹并赋予执行权限
-if sudo wget -L -O "$download_path/$script_name"  "$main_script_url"  &&  sudo chmod +x "$download_path/$script_name" ; then
-    echo "所需依赖已全部安装成功，此脚本即将自动删除"
+if sudo wget -L -O  "$download_path/$script_name"   "$main_script_url"   &&  sudo chmod +x  "$download_path/$script_name"  ; then
+    echo "查看系统信息脚本已下载，此脚本即将自动删除"
     echo "请输入-> sudo $download_path/$script_name <-进行运行所需脚本"
     rm "$0" # 删除当前脚本
 else
-    echo "下载脚本失败，依赖安装成功，请检查下载失败原因!"
+    echo "下载脚本失败，请检查下载失败原因!"
     rm "$0" # 删除当前脚本
 fi
