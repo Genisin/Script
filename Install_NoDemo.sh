4a77b5d9d574860176c49be83e34bc532ac8310e
Genisin <1841542034@qq.com>
2023-08-19 13:44:25 +0800

    v1

--- Install_NoDemo.sh
+++ Install_NoDemo.sh
@@ -8,13 +8,18 @@ script_name="脚本名.sh"
 #脚本下载地址 
 main_script_url= "主脚本的文件的原始内容链接"
 
+#字体颜色定义
+orange='\033[33m'
+green='\033[32m'
+plain='\033[0m'
+
 #创建主脚本下载路径
 mkdir -p /root/data/script
 download_path="/root/data/script"
 
 #下载主脚本到指定文件夹并赋予执行权限
 if sudo wget -L -O "$download_path/$script_name"  "$main_script_url"  &&  sudo chmod +x "$download_path/$script_name" ; then
-    echo "请输入-> sudo $download_path/$script_name <-进行运行所需脚本"
+    echo "请输入-> ${green}sudo $download_path/$script_name{plain} <-进行运行"
     rm "$0" # 删除当前脚本
 else
     echo "下载脚本失败，请再次尝试！（若多次尝试仍无法下载，建议手动下载）!"
