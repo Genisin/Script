b555f442cc128ad2f3e2b94132f8e2f2cabc38b4
Genisin <1841542034@qq.com>
2023-09-19 10:33:10 +0800

    v1

--- Log_Clean/main.sh
+++ Log_Clean/main.sh
@@ -4,10 +4,18 @@
 #crontab -e    #打开系统的定时任务列表
 #分（0-59），时（0-23），日（1-31），月（1-12），星期（0-6，其中0表示星期天）
 #0 0 * * 0 /bin/bash /path/to/your/script.sh  #设置清理时间
-
-log_dir="/var/log"
-max_log_size=1048576  # 1MB 
+#路径： /var/log
+#大小：1MB： 1048576 全清： 0 
+log_dir="/"
+max_log_size=100   # 100Kb
 logpath="/var/log/logclean.log"
+
+# 更新软件包并清理多余软件包
+if apt update -y && apt full-upgrade -y && sudo apt dist-upgrade && apt autoremove -y && apt autoclean -y; then
+    echo "$(date +'%Y-%m-%d %H:%M:%S') - 已更新软件包并清理多余软件包" >> "$logpath"
+else
+    echo "$(date +'%Y-%m-%d %H:%M:%S') - 软件包更新和清理失败" >> "$logpath"
+fi
 
 # 使用 find 命令获取所有子文件夹中的 .log 文件并将结果保存到 log_files 数组
 log_files=()
