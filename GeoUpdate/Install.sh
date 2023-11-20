740e20754f5dbc2f39b46e3018a5e16b1c195009
Script <script@example.com>
2023-11-20 18:07:31 +0800

    v1

---
+++ GeoUpdate/Install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 脚本名字
+script_name="GeoUpdate.sh"
+# 此链接为主脚本下载地址 
+main_script_url="https://raw.githubusercontent.com/Genisin/script/GeoUpdate/main.sh"
+
+#创建主脚本下载路径
+mkdir -p /root/data/script
+download_path="/root/data/script"
+
+#字体颜色定义
+orange='\033[33m'
+green='\033[32m'
+plain='\033[0m'
+
+#下载主脚本到指定文件夹并赋予执行权限
+if sudo wget -L -O "$download_path/$script_name" "$main_script_url" && sudo chmod +x "$download_path/$script_name"; then
+    echo -e "请输入-> ${green}sudo $download_path/$script_name${plain} <-进行运行"
+    rm "$0" # 删除当前脚本
+else
+    echo -e "${orange}下载脚本失败，请再次尝试！${plain}"
+    rm "$0" # 删除当前脚本
+fi
---
+++ GeoUpdate/main.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 设置文件下载地址
+GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
+GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+
+# 设置目标目录
+TARGET_DIR="/usr/local/x-ui/bin/"
+
+# 下载并替换 geoip.dat
+wget -O "${TARGET_DIR}geoip.dat" "${GEOIP_URL}"
+
+# 下载并替换 geosite.dat
+wget -O "${TARGET_DIR}geosite.dat" "${GEOSITE_URL}"
