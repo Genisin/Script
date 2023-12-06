c20810d7d4dfb9298656f75129b01d10d1639b4a
Script <script@example.com>
2023-12-06 10:53:56 +0800

    v1

--- SystemInfo/main.sh
+++ SystemInfo/main.sh
@@ -19,20 +19,23 @@ cpu_cores=$(nproc)
 # 获取CPU型号和主频
 cpu_model=$(lscpu | grep "Model name" | awk -F ": " '{sub(/^[ \t]+/, "", $2); print $2}')
 
-# 获取内存大小和占用（MB）
-memory_size=$(free -m | awk '/Mem:/ {print $2}')
-used_memory=$(free -m | awk '/Mem:/ {print $3}')
-memory_usage=$(free | awk '/Mem:/ {printf "% 5.2f", ($3/$2)*100}')
-
 # 获取硬盘大小和占用情况
 disk_size=$(df -h | awk '/\/$/ {print $2}')
 used_disk=$(df -h | awk '/\/$/ {print $3}')
 disk_usage=$(df | awk '/\/$/ {printf "% 5.2f", ($3/$2)*100}')
+format_disk_usage=$(df | awk '/\/$/ {printf "% 5.2f", $3/1024}') # 格式化硬盘占用为GB
+
+# 获取内存大小和占用（MB）
+memory_size=$(free -m | awk '/Mem:/ {print $2}')
+used_memory=$(free -m | awk '/Mem:/ {print $3}')
+memory_usage=$(free | awk '/Mem:/ {printf "% 5.2f", ($3/$2)*100}')
+format_memory_usage=$(free -m | awk '/Mem:/ {printf "% 5.2f", $3})  # 格式化内存占用为MB
 
 # 获取虚拟内存大小和占用情况
 swap_size=$(free -h | awk '/Swap/ {print $2}')
 used_swap=$(free -h | awk '/Swap/ {print $3}')
 swap_usage=$(free | awk '/Swap/ {printf "% 5.2f", ($3/$2)*100}')
+format_swap_usage=$(free -h | awk '/Swap/ {printf "% 5.2f", $3/1024}') # 格式化虚拟内存占用为GB
 
 #********IP信息********#
 ipv4_address=$(curl -s ipv4.ip.sb)
@@ -58,6 +61,6 @@ echo "IP归属地:  $country $city"
 echo "IPv4运营商: $isp_info"
 echo -e "${GREEN}======硬件信息=======${NC}"
 echo "CPU型号： $cpu_model @  $cpu_cores 核"
-echo "硬盘占用/硬盘大小：   $format_disk_usage/$disk_size     占用率：$disk_usage%"
-echo "内存占用/内存大小：$format_memory_usage/$memory_size MB    占用率：$memory_usage%"
-echo "虚拟内存占用/大小：$used_swap/$swap_size MB    占用率：$swap_usage%"
+echo "硬盘占用/硬盘大小：   $format_disk_usage GB / $disk_size     占用率：$disk_usage%"
+echo "内存占用/内存大小：$format_memory_usage MB / $memory_size MB    占用率：$memory_usage%"
+echo "虚拟内存占用/大小：$used_swap / $format_swap_usage GB    占用率：$swap_usage%"
